ded0639898c3cd1b22af3e1a62edb2457e444a8c
ZoyaNaaz <123814747+ZoyaNaaz@users.noreply.github.com>
2026-05-26 10:19:45 +0000

    Update Faq section

--- script.js
+++ script.js
@@ -45,7 +45,20 @@ const line2 = document.getElementById("line2");
 const line3 = document.getElementById("line3");
 const line4 = document.getElementById("line4");
 
-setupIntersectionObserver(line1, true, 0.15)
-setupIntersectionObserver(line2, false, 0.15)
-setupIntersectionObserver(line3, true, 0.15)
-setupIntersectionObserver(line4, true, 0.8)
+setupIntersectionObserver(line1, true, 0.15);
+setupIntersectionObserver(line2, false, 0.15);
+setupIntersectionObserver(line3, true, 0.15);
+setupIntersectionObserver(line4, true, 0.8);
+
+const dtElement = document.querySelectorAll('dt');
+
+dtElement.forEach((element) =>{
+    element.addEventListener('click', () => {
+        const ddId = element.getAttribute('aria-controls');
+        const ddElement = document.getElementById(ddId);
+        const ddArrowIcon = element.querySelector('i');
+
+        ddElement.classList.toggle('hidden');
+        ddArrowIcon.classList.toggle('rotate-180');
+    })
+})
